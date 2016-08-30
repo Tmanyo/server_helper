37f536548517ffba3e9f5b7c1871a3bde3ff9cfc
Tmanyo <trent@lasich.com>
2016-08-29 22:40:06 -0400

    Good server helper.
    This mod adds a new look and a slightly altered code to the admin pick.
    It also provides an automatic response to certain keywords.  It can be a
    good help to players who struggle to understand the rules.

---
+++ init.lua
@@ -0,0 +1,64 @@
+-- Code for the admin_pick from maptools by Calinou, has been altered.
+minetest.register_tool("server_helper:admin_pick", {
+  description = "Admin Pick",
+  groups = {not_in_creative_inventory = 1},
+  range = 15,
+  inventory_image = "admin_pick.png",
+  tool_capabilities = {
+    full_punch_interval = .001,
+    max_drop_level = 3,
+    groupcaps = {
+		  unbreakable = {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			fleshy =      {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			choppy =      {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			bendy =       {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			cracky =      {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			crumbly =     {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			snappy =      {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+		},
+    damage_groups = {fleshy = 1000},
+  },
+})
+
+-- These watch for certain keywords or phrases and make a response.
+minetest.register_on_chat_message(function(name,message)
+  if string.match(message, "grief" or "griefing" or "griefed") then
+    minetest.chat_send_all("<The All Seeing Eye> Griefing is not permitted and will not be allowed!")
+  end
+end)
+
+minetest.register_on_chat_message(function(name,message)
+  if string.match(message, "cussing") or string.match(message, "cursing") or string.match(message, "bad word") then
+    minetest.chat_send_all("<The All Seeing Eye> Bad language is not acceptable.")
+  end
+end)
+
+minetest.register_on_chat_message(function(name,message)
+  if message == "who is The All Seeing Eye" or message == "who is the all seeing eye" or message == "The All Seeing Eye?" or message == "who is The All Seeing Eye?" or message == "who is the all seeing eye?" or message == "Who is the all seeing eye?" or message == "Who is The All Seeing Eye?" then
+    minetest.chat_send_all("<The All Seeing Eye> I am a server moderator created by Tmanyo.")
+  end
+end)
+
+minetest.register_on_chat_message(function(name,message)
+  if message == "can i be a mod" or message == "can i be a mod?" or message == "Can I be a mod?" or message == "can i be an admin" or message == "can i be an admin?" or message == "Can I be an admin?" or message == "can i have more privs" then
+    minetest.chat_send_player(name, "<The All Seeing Eye> You need to ask server administration.")
+  end
+end)
+
+minetest.register_on_chat_message(function(name,message)
+  if string.match(message, "dumb") or string.match(message, "stupid") or string.match(message, "ugly") or string.match(message, "idiot") then
+    minetest.chat_send_all("<The All Seeing Eye> Shots fired!  Those are fighting words...")
+  end
+end)
+
+minetest.register_on_chat_message(function(name,message)
+  if string.match(message, "favorite color") or string.match(message, "color") or string.match(message, "colour") then
+    minetest.chat_send_all("<The All Seeing Eye> My favorite color is yellow.")
+  end
+end)
+
+minetest.register_on_chat_message(function(name,message)
+  if string.match(message, "favorite food") or string.match(message, "food") or string.match(message, "meal") then
+    minetest.chat_send_all("<The All Seeing Eye> My favorite food is Chicken.")
+  end
+end)
