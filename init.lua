4fafa707cd710b94519605e3c4db0fdc1d04941d
Tmanyo <trent@lasich.com>
2016-10-10 20:34:26 -0400

    Fixed crash on I am stuck.

--- init.lua
+++ init.lua
@@ -45,7 +45,7 @@ end)
 
 minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
-   server_helper.players[name]= {shout = 0, location = 27,}
+   server_helper.players[name] = {shout = 0, location = 27,}
 end)
 
 -- This watches for all caps usage and warns 4 times and kicks on the 5th.
@@ -112,6 +112,9 @@ minetest.register_on_chat_message(function(name,message)
         elseif message == "yes" or message == "Yes" then
           if respawn == 1 then
             local pos = minetest.setting_get_pos("static_spawnpoint")
+               if pos == nil then
+                    local pos = 0,0,0
+               end
             local player = minetest.get_player_by_name(name)
             player:setpos(pos)
             minetest.chat_send_player(name, "<The All Seeing Eye> There you are!")
